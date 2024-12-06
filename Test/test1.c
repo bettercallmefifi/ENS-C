4b2ec06223412ab8cc7dc82157289498328f0609
bettercallmefifi <ferdaouskai@gmail.com>
2024-12-06 09:40:27 +0100

    done

--- Test/test1.c
+++ Test/test1.c
@@ -3,26 +3,31 @@
 int main()
 {
 	int n;
+	printf("=== Menu: ===\n");
+
+	printf("1.Royal Cheese\n2.Mc Delux\n3.Mc Bacon\n4.Big Mac\n");
 
 	printf("votre choix:");
+
 	scanf("%d",&n);
 
 	switch(n)
 	{
 		case 1:
-		printf("Royal Chesse");
+		printf("Royal Chesse\n");
 		break;
 		case 2:
-		printf("Mc Deluxe");
+		printf("Mc Deluxe\n");
 		break;
 		case 3:
-		printf("Mc Bacon");
+		printf("Mc Bacon\n");
 		break;
 		case 4:
-		printf("Big Mac");
+		printf("Big Mac\n");
 		break;
 		default:
-		printf("je n'ai aucune Choix!");
+		printf("aucune Choix!\n");
 		break;
 	}
+	return (0);
 }
