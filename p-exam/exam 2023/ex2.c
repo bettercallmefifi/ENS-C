b5974d880305edfe4e9f7784ead9365fa9004419
bettercallmefifi <ferdaouskai@gmail.com>
2025-01-27 00:23:04 +0100

    done

--- p-exam/exam 2023/ex2.c
+++ p-exam/exam 2023/ex2.c
@@ -1,32 +1,54 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
+
 int resoudre2(int a,int b,int c,float *X1, float *X2)
 {
 	float delta;
 
-	if(a == 0) // bx+c = 0
+	if(a == 0 && b == 0 && c == 0)
+		return(-1);
+	else if(a == 0 && b == 0 && c != 0)
+		return(0);
+	else if(a == 0 && b!= 0 && c == 0)
 	{
-		if(b == 0)// c = 0
-		{
-			if(c == 0)
-				return(-1);
-			else
-				return(0);
-		}
-		else{//bx+c = 0
-			*X1= (float)(-c/b);
-			return(1);
-		}
+		*X1 = 0;
+		return(1);
 	}
-	else{ //ax^2 + bx + c =0
+	else if(a != 0 && b == 0 && c == 0)
+	{
+		*X1 = 0;
+		return(1);
+	}
+	else if(a == 0 && b != 0 && c != 0)
+	{
+		*X1= (float)-c / b;
+		return(0);
+	}
+	else if( a != 0 && b != 0 && c == 0)
+	{
+		*X1 = 0;
+		*X2 = (float) -b / a;
+		return(2);
+	}
+	else if(a != 0 && b == 0 && c != 0)
+	{
+		if((c > 0 && a < 0)||(c < 0 && a > 0))
+		{
+			*X1 = sqrt((float)-c/a);
+			*X2 = - sqrt((float)-c/a);
+			return(2);
+		}
+		else
+			return(0);
+	}
+	else{
 		delta = pow(b,2)- 4 * a * c;
 
 		if(delta < 0)
 			return (0);
 		else if(delta == 0)
 			{
-				*X1= (float)(-b/ 2 * a);
+				*X1= (float)(-b)/( 2 * a);
 				return(1);
 			}
 		else if(delta > 0)
@@ -46,9 +68,9 @@ int main() {
 
     int result = resoudre2(a, b, c, &x1, &x2);
     if (result == -1) {
-        printf("Équation indéterminée.\n");
+        printf("tout x est une solution\n");
     } else if (result == 0) {
-        printf("Pas de solution réelle.\n");
+        printf("Pas de solution\n");
     } else if (result == 1) {
         printf("Une solution : x = %.2f\n", x1);
     } else if (result == 2) {
